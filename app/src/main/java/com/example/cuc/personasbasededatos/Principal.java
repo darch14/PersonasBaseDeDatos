21abd232b771f6f0e7abc894e7c3d6bc3d26acf6
David Rodriguez <drodrigu28@cuc.edu.co>
2017-05-13 11:55:16 -0500

    Registro en la base de datos terminada

--- app/src/main/java/com/example/cuc/personasbasededatos/Principal.java
+++ app/src/main/java/com/example/cuc/personasbasededatos/Principal.java
@@ -1,13 +1,48 @@
 package com.example.cuc.personasbasededatos;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Adapter;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class Principal extends AppCompatActivity {
+    private ListView opciones;
+    private String[] opc;
+    private ArrayAdapter adapter;
+    private Intent i;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_principal);
+
+        opciones=(ListView)findViewById(R.id.lvOpciones);
+        opc=getResources().getStringArray(R.array.opciones_principal);
+        adapter=new ArrayAdapter(this,android.R.layout.simple_list_item_1,opc);
+        opciones.setAdapter(adapter);
+
+        opciones.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                switch (position){
+                    case 0:
+                        i = new Intent(Principal.this, Registrar.class);
+                        startActivity(i);
+                        break;
+                    case 1:
+                        i = new Intent(Principal.this, Listado.class);
+                        startActivity(i);
+                        break;
+                    case 2:
+                        i = new Intent(Principal.this, ListadoPersonalizado.class);
+                        startActivity(i);
+                        break;
+                }
+            }
+        });
     }
 }
